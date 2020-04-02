fffd10b60a7b5cc2de4647f827d2af36afabb7b2
Somya Singh <singh.somya1509@gmail.com>
2020-04-03 00:54:21 +0530

    Updates

---
+++ precache-manifest.a5f5af472f30b97c84708b0fbf51e484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa28fba51259e0032827723d4bc9715",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "659c0534de46388bbb26",
+    "url": "/monsters-rolodex/static/css/main.40cb1834.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "659c0534de46388bbb26",
+    "url": "/monsters-rolodex/static/js/main.e1228135.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
